0a34e17fb5b171c3ac28204417aad4cb8fb8b47a
Ankit Somani <ankit.m.somani@gmail.com>
2021-03-16 22:24:35 -0400

    creating function to get data from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,38 @@ var currentWindSpeed = $("#wind-speed");
 var currentUVIndex = $("#uv-index");
 var sCity = [];
 
+var APIKey = "a0aca8a89948154a4182dcecc780b513";
+
 // SEARCH CITY TO SEE IT EXISTS FROM STORAGE //
-function find(city) {
+function find(c) {
     for (var i = 0; i < sCity.length; i++) {
-        if (city.toUpperCase() === sCity[i]) {
-            return -1;   
+        if (c.toUpperCase() === sCity[i]) {
+            return -1;
         }
     }
     return 1;
+}
+
+// DISPLAY CURRENT & FUTURE WEATHER //
+
+function displayWeather(event) {
+    event.preventDefault();
+    if (searchCity.val().trim() !== "") {
+        city = searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+// GET DATA FROM SERVER //
+function currentWeather(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APID=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+        var weathericon = response.weather[0].icon;
+        var iconURL = "https://openweathermap.org/img/wn/" + weathericon + "@2x.png";
+        var date = new Date(response.dt * 1000).toLocaleDateString();
+        $(currentCity).html(response.name + "(" + date + ")" + "<img src" + iconURL + ">");
+    })
 }
