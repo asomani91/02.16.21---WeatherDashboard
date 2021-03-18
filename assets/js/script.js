3a49e6bacce0e95f742469c29d04780cf64cbfb0
Ankit Somani <ankit.m.somani@gmail.com>
2021-03-17 21:46:36 -0400

    creating function to return uv index response

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,3 +75,13 @@ function currentWeather(city) {
     });
 }
 
+// RETURN UV INDEX RESPONSE //
+function UVIndex(ln, lt) {
+    var uvqURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lt + "&lon=" + ln;
+    $ajax({
+        url: uvqURL
+        method: "GET"
+    }).then(function (response) {
+        $(currentUVIndex).html(response.value);
+    });
+}
